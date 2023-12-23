e33d840da8358f025fe5383daf247c0ae55eeeb3
Tofail Ahmed Sayem <atofail50@gmail.com>
2023-12-24 00:06:38 +0600

    error pattern re-formatting

--- validation/auth.js
+++ validation/auth.js
@@ -3,8 +3,9 @@ const { check, validationResult } = require("express-validator");
 exports.runValidation = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
+      let errorsList=errors.array().map((error)=>error.msg)
     return res.status(400).json({
-      errors: errors.array(),
+      errors: errorsList,
     });
   }
   next(); // Move to the next middleware or route handler if there are no errors
